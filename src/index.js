b21965fbc6c32faec3086af4727d8908ccd15cab
Vitali Satsura <vitali.satsura@gmail.com>
2019-09-27 19:21:49 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  if (knowsProgramming) {
+    return Math.ceil(800 / config[focus]);
+  }
+  return Math.ceil(1300 / config[focus]);
+};
